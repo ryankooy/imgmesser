5b975bf333d0497d0b9a75be8c7c40b646a8ff71
Ryan Kooy <rwkooy@gmail.com>
2026-07-29 15:47:58 -0400

    manual deploy updates
    Updated the manual deployment scripts to more closely match the Docker deployment commands in that of the GitHub workflow file.

--- deploy_files/run.sh
+++ deploy_files/run.sh
@@ -23,21 +23,12 @@ else
 fi
 
 if [ "${deploy}" = 'app' ] || [ "${deploy}" = 'all' ]; then
-    [ -z "${CLIENT_CONTAINER}" ] && { echo 'CLIENT_CONTAINER variable not set' >&2; exit 1; }
-    [ -z "${CLIENT_REPO}" ] && { echo 'CLIENT_REPO variable not set' >&2; exit 1; }
+    [ -z "${FRONTEND_REPO}" ] && { echo 'FRONTEND_REPO variable not set' >&2; exit 1; }
     [ -z "${DOMAIN}" ] && { echo 'DOMAIN variable not set' >&2; exit 1; }
     [ -z "${EMAIL}" ] && { echo 'EMAIL variable not set' >&2; exit 1; }
 
-    if docker container inspect "${CLIENT_CONTAINER}" >/dev/null 2>&1 && \
-       [ "$(docker container inspect -f '{{.State.Running}}' ${CLIENT_CONTAINER})" = 'true' ]
-    then
-        echo 'Stopping and removing the existing client container...'
-        docker stop "${CLIENT_CONTAINER}"
-        docker rm "${CLIENT_CONTAINER}"
-    fi
-
-    echo 'Pulling the latest client image...'
-    docker pull "${DOCKER_USER}"/"${CLIENT_REPO}":latest
+    echo 'Pulling the latest frontend image...'
+    docker compose pull app
 
     echo 'Running the certbot service and creating dummy certificates...'
     docker compose run --rm --entrypoint " \
@@ -47,7 +38,7 @@ if [ "${deploy}" = 'app' ] || [ "${deploy}" = 'all' ]; then
             -out /etc/letsencrypt/live/${DOMAIN}/fullchain.pem -subj '/CN=localhost'" \
         certbot
 
-    echo 'Running the client service (and nginx) in a new container...'
+    echo 'Running the frontend service (and nginx) in a new container...'
     docker compose --profile nginx up --detach
 
     echo 'Running certbot again separately to create real certificates...'
@@ -56,28 +47,19 @@ if [ "${deploy}" = 'app' ] || [ "${deploy}" = 'all' ]; then
             -d ${DOMAIN} --email ${EMAIL} --agree-tos --no-eff-email --force-renewal" \
         certbot
 
-    echo 'Reloading nginx in the running client container...'
+    echo 'Reloading nginx in the running frontend container...'
     docker compose exec app nginx -s reload
 fi
 
 if [ "${deploy}" = 'api' ] || [ "${deploy}" = 'all' ]; then
-    [ -z "${API_CONTAINER}" ] && { echo 'API_CONTAINER variable not set' >&2; exit 1; }
     [ -z "${API_DOCKER_IP}" ] && { echo 'API_DOCKER_IP variable not set' >&2; exit 1; }
-    [ -z "${API_REPO}" ] && { echo 'API_REPO variable not set' >&2; exit 1; }
+    [ -z "${BACKEND_REPO}" ] && { echo 'BACKEND_REPO variable not set' >&2; exit 1; }
     [ -z "${POSTGRES_USER}" ] && { echo 'POSTGRES_USER variable not set' >&2; exit 1; }
     [ -z "${POSTGRES_PASSWORD}" ] && { echo 'POSTGRES_PASSWORD variable not set' >&2; exit 1; }
     [ -z "${POSTGRES_DB}" ] && { echo 'POSTGRES_DB variable not set' >&2; exit 1; }
 
-    if docker container inspect "${API_CONTAINER}" >/dev/null 2>&1 && \
-       [ "$(docker container inspect -f '{{.State.Running}}' ${API_CONTAINER})" = 'true' ]
-    then
-        echo 'Stopping and removing the existing API container...'
-        docker stop "${API_CONTAINER}"
-        docker rm "${API_CONTAINER}"
-    fi
-
-    echo 'Pulling the latest API image...'
-    docker pull "${DOCKER_USER}"/"${API_REPO}":latest
+    echo 'Pulling the latest backend image...'
+    docker compose pull api
 
     export API_DOCKER_IP="${API_DOCKER_IP}"
     export POSTGRES_USER="${POSTGRES_USER}"
@@ -85,10 +67,13 @@ if [ "${deploy}" = 'api' ] || [ "${deploy}" = 'all' ]; then
     export POSTGRES_DB="${POSTGRES_DB}"
 
     if [ "${deploy}" = 'api' ]; then
-        echo 'Running the API service in a new container...'
+        echo 'Running the backend service in a new container...'
         docker compose up --detach api
     elif [ "${deploy}" = 'all' ]; then
         echo 'Running all services...'
         docker compose up --detach
+
+        # Prune stale image layers
+        docker image prune -f
     fi
 fi
--- manual_deploy.sh
+++ manual_deploy.sh
@@ -21,13 +21,11 @@ function die() {
     [ "$1" -ne 0 ] && { printf "ERROR: %s\n" "$2" >&2; exit 1; }
 }
 
-api_repo="${PROJECT}-api"
-api_container="${api_repo}-1"
-client_repo="${PROJECT}-app"
-client_container="${client_repo}-1"
+backend_repo="${PROJECT}-api"
+frontend_repo="${PROJECT}-app"
 copy_cfgs=false
-deploy_api=false
-deploy_client=false
+deploy_backend=false
+deploy_frontend=false
 deploy_all=false
 no_build=false
 svc=all
@@ -46,49 +44,45 @@ done
 if [ "${no_build}" = false ]; then
     case "${svc}" in
         all) deploy_all=true;;
-        app) deploy_client=true;;
-        api) deploy_api=true;;
+        app) deploy_frontend=true;;
+        api) deploy_backend=true;;
         *) die 1 'Unknown service specified';;
     esac
 
-    # Build image(s) using `compose.yaml`
-    if [ "${deploy_all}" = true ]; then
-        echo 'Building images'
-        docker compose build
-    else
-        echo 'Building image'
-        docker compose build "${svc}"
-    fi
-    die "$?" 'Failed to build image(s)'
+    echo 'Building images...'
 
-    # Tag and push the API image
-    if [ "${deploy_api}" = true ] || [ "${deploy_all}" = true ]; then
-        echo 'Tagging and pushing API image'
-        docker tag "${api_repo}":latest "${DOCKER_USER}"/"${api_repo}":latest
-        docker push "${DOCKER_USER}"/"${api_repo}":latest
+    # Build, tag, and push the backend image
+    if [ "${deploy_backend}" = true ] || [ "${deploy_all}" = true ]; then
+        docker build --target backend-image -t "${backend_repo}" .
+        die "$?" 'Failed to build backend image'
+
+        echo 'Tagging and pushing backend image...'
+        docker tag "${backend_repo}":latest "${DOCKER_USER}"/"${backend_repo}":latest
+        docker push "${DOCKER_USER}"/"${backend_repo}":latest
     fi
 
-    # Tag and push the client app image
-    if [ "${deploy_client}" = true ] || [ "${deploy_all}" = true ]; then
-        echo 'Tagging and pushing client app image'
-        docker tag "${client_repo}":latest "${DOCKER_USER}"/"${client_repo}":latest
-        docker push "${DOCKER_USER}"/"${client_repo}":latest
+    # Build, tag, and push the frontend image
+    if [ "${deploy_frontend}" = true ] || [ "${deploy_all}" = true ]; then
+        docker build --target frontend-image -t "${frontend_repo}" .
+        die "$?" 'Failed to build frontend image'
+
+        echo 'Tagging and pushing frontend app image...'
+        docker tag "${frontend_repo}":latest "${DOCKER_USER}"/"${frontend_repo}":latest
+        docker push "${DOCKER_USER}"/"${frontend_repo}":latest
     fi
 fi
 
 # Copy config files to the EC2 instance
 if [ "${copy_cfgs}" = true ]; then
-    echo 'Copying files to server'
+    echo 'Copying files to server...'
     scp -r deploy_files/* "${EC2_INSTANCE_ALIAS}":~
     ssh "${EC2_INSTANCE_ALIAS}" chmod 700 run.sh
 fi
 
-# On the EC2 Instance, remove old containers, pull fresh
-# images, and run new containers
-echo 'Deploying to server'
+# On the EC2 Instance, pull fresh images and run new containers
+echo 'Deploying to server...'
 ssh "${EC2_INSTANCE_ALIAS}" " \
-    API_CONTAINER=${api_container} API_DOCKER_IP=${API_DOCKER_IP} API_REPO=${api_repo} \
-        CLIENT_CONTAINER=${client_container} CLIENT_REPO=${client_repo} \
+    API_DOCKER_IP=${API_DOCKER_IP} BACKEND_REPO=${backend_repo} FRONTEND_REPO=${frontend_repo} \
         DOCKER_USER=${DOCKER_USER} DOMAIN=${DOMAIN} EMAIL=${EMAIL} \
         POSTGRES_USER=${POSTGRES_USER} POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
         POSTGRES_DB=${POSTGRES_DB} PROJECT=${PROJECT} \
